fe4ba0b818b55f8e15b37f857d7cc80e14e37796
jjdeforrest <jjdeforrest@users.noreply.github.com>
2020-02-16 19:24:26 -0800

    update

--- src/test/java/com/example/hobbyist/hobbyist/models/ApplicationUserTest.java
+++ src/test/java/com/example/hobbyist/hobbyist/models/ApplicationUserTest.java
@@ -1,5 +1,43 @@
+package com.example.hobbyist.hobbyist.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationUserTest {
+
+
+    ApplicationUser applicationUser;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        applicationUser = new ApplicationUser("TestName", "TestArtist", "hello", "example.com");
+    }
+
+    @Test
+    public void getlastname() {
+
+        assertEquals("TestName", applicationUser.lastName);
+    }
+
+    @Test
+    public void getfirstname()
+    {
+        assertEquals("TestArtist", applicationUser.firstName);
+    }
+
+    @Test
+    public void getusername() {
+        assertEquals("hello", applicationUser.username);
+    }
+
+
+    @Test
+    public void getpassword() {
+
+        assertEquals("example.com", applicationUser.password);
+    }
+
 
 }
